933395aa311b6a71cbfa0f38d6f294c3ca5e2e3f
Tim <tmetcalfe89@gmail.com>
2021-09-15 12:52:50 -0700

    Created basic side nav
    To Do:
    * Add delete functionality to groups.
    * Add confirm for delete on groups and entries.
    * Unify verbiage (item or entry?).
    * Compartmentalize some of these sub-components better.

---
+++ src/components/GroupMaker.js
@@ -0,0 +1,38 @@
+import React, { useRef, useState } from 'react'
+import { Modal, Button, Form } from "react-bootstrap"
+
+function GroupMaker({ shown, hide, save }) {
+  const [name, setName] = useState("");
+  const ref = useRef();
+
+  const submit = () => {
+    save(name);
+    hide();
+  }
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    submit();
+  }
+
+  return (
+    <Modal show={shown} onExited={() => setName("")} onShow={() => ref.current.focus()}>
+      <Modal.Header closeButton onHide={hide}>
+        <Modal.Title>Add new group</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={formSubmit}>
+          <Form.Group className="mb-3">
+            <Form.Control ref={ref} value={name} onChange={(e) => setName(e.target.value)} />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={hide}>Close</Button>
+        <Button variant="primary" onClick={() => submit(name)}>Save changes</Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
+
+export default GroupMaker
---
+++ src/components/ItemMaker.js
@@ -0,0 +1,51 @@
+import React, { useRef, useState } from 'react'
+import { Modal, Button, Form } from "react-bootstrap"
+
+function ItemMaker({ shown, hide, save, types = ["item", "block"] }) {
+  const [name, setName] = useState("");
+  const [type, setType] = useState("invalid");
+  const ref = useRef()
+
+  const submit = () => {
+    if (type === "invalid") return;
+    save(name, type);
+    hide();
+  }
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    submit();
+  }
+
+  const resetForm = () => {
+    setName("");
+    setType("invalid");
+  }
+
+  return (
+    <Modal show={shown} onExited={resetForm} onShow={() => ref.current.focus()}>
+      <Modal.Header closeButton onHide={hide}>
+        <Modal.Title>Add new item</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={formSubmit}>
+          <Form.Group className="mb-3">
+            <Form.Control ref={ref} value={name} onChange={(e) => setName(e.target.value)} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Select value={type} onChange={(e) => setType(e.target.value)}>
+              <option value="invalid">Type</option>
+              {types.map(type => <option value={type}>{type}</option>)}
+            </Form.Select>
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={hide}>Close</Button>
+        <Button variant="primary" onClick={() => submit(name)}>Save changes</Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
+
+export default ItemMaker
