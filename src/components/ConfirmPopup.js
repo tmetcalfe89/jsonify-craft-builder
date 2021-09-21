4ca7d857ad3060e3f0f0b9cb1d3e0332f072beb2
Tim <tmetcalfe89@gmail.com>
2021-09-21 12:16:34 -0700

    Added confirmation to deletion process for items and groups
    Also added a generic ConfirmPopup. Useful for future things. Learned that React attempts to evaluate out a function when you pass said function to a state variable's setter. To fix this, just return the function from a function to use the function as the value of the state variable. Easy, right? Check out App.js in this commit, line 16.

---
+++ src/components/ConfirmPopup.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Modal, Button } from 'react-bootstrap'
+
+function ConfirmPopup({ shown, hide, confirm }) {
+  const confirmMe = () => {
+    hide()
+    confirm()
+  }
+
+  return (
+    <Modal show={shown}>
+      <Modal.Body>
+        Are you sure?
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant='secondary' onClick={hide}>No</Button>
+        <Button variant='primary' onClick={confirmMe}>Yes</Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
+
+export default ConfirmPopup
