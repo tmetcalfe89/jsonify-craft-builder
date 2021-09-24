45af14522ad3e5526df7b26677842e276554ea11
Tim <tmetcalfe89@gmail.com>
2021-09-23 17:49:41 -0700

    Wrapped AutoForm in an optional Form
    Also gave it a submit function with a default of preventDefault on the event.

--- src/components/AutoForm/index.js
+++ src/components/AutoForm/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Form } from 'react-bootstrap'
 import FieldTypes from './FieldTypes'
 
-function AutoForm ({ description = {}, data = {}, setData = () => {} }) {
+function AutoForm ({
+  description = {},
+  data = {},
+  setData = () => {},
+  submit = e => e.preventDefault()
+}) {
   const updateField = (key, value) => {
     setData({
       ...data,
@@ -9,17 +15,26 @@ function AutoForm ({ description = {}, data = {}, setData = () => {} }) {
     })
   }
 
-  return Object.entries(description).map(([id, fieldDescription]) => {
-    const FieldElement = FieldTypes[fieldDescription.type]
-    return (
-      <FieldElement
-        description={fieldDescription}
-        value={data[id]}
-        updateValue={value => updateField(id, value)}
-        key={`field-element-${id}`}
-      />
-    )
-  })
+  return (
+    <Form onSubmit={submit}>
+      {Object.entries(description).map(([id, fieldDescription], i, arr) => {
+        const FieldElement = FieldTypes[fieldDescription.type]
+        const { label, note } = fieldDescription
+        return (
+          <Form.Group className={i === arr.length - 1 ? null : 'mb-3'}>
+            {label && <Form.Label>{label}</Form.Label>}
+            <FieldElement
+              description={fieldDescription}
+              value={data[id]}
+              updateValue={value => updateField(id, value)}
+              key={`field-element-${id}`}
+            />
+            {note && <Form.Text>{note}</Form.Text>}
+          </Form.Group>
+        )
+      })}
+    </Form>
+  )
 }
 
 export default AutoForm
